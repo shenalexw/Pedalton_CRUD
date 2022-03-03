64bbb5f3459e3585e6e73fb4dd9bbac60d3245b4
Collin Sidler <sidlerc@oregonstate.edu>
2022-03-03 14:24:33 -0800

    dynamic FK drop downs and a seconds-to-time converter for Video duration

--- routes/videos.js
+++ routes/videos.js
@@ -1,3 +1,8 @@
+// Citation for the seconds-to-time converter
+// Date: 3/3/22
+// Adapted from:
+// https://stackoverflow.com/questions/1322732/convert-seconds-to-hh-mm-ss-with-javascript
+
 // Connect to Database
 var db = require('../database/db-connector')
 
@@ -7,8 +12,24 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   let query1 = "SELECT * FROM Videos;";               // Define our query
+  let query2 = "SELECT instructorID FROM Instructors;";
+
+  let videoData;
+  let instructorIDs;
+
+  // Query 1 will get all of the rows from the Videos table
   db.pool.query(query1, function(error, rows, fields){    // Execute the query
-      res.render('videos', {data: rows});                  // Render the index.hbs file, and also send the renderer
+    videoData = rows;
+
+    // Query 2 will get the instructorIDs from the Instructors table to dynamically populate the Instructor ID input in Add Video form
+    db.pool.query(query2, function(error, rows, fields){
+      instructorIDs = rows;
+
+      // Sort the instructorIDs so they are in ascending order
+      instructorIDs.sort((a, b) => a.instructorID - b.instructorID)
+
+      res.render('videos', {data: videoData, instructorIDs: instructorIDs});                  // Render the index.hbs file, and also send the renderer
+    })
   })  
 })
 
@@ -21,8 +42,11 @@ router.post('/add-video', function(req, res){
     instructorID = data['addInstructorID'];
   }
 
+  // Converts user-inputted seconds into a time string (hh:mm:ss)
+  const convertedDuration = new Date(data['addDuration'] * 1000).toISOString().slice(11, 19);
+
   // Create the query and run it on the database
-  query1 = `INSERT INTO Videos(instructorID, name, description, type, difficulty, duration) VALUES (NULLIF('${instructorID}', ''), '${data['addName']}', '${data['addDescription']}', '${data['addType']}', '${data['addDifficulty']}', '${data['addDuration']}')`;
+  query1 = `INSERT INTO Videos(instructorID, name, description, type, difficulty, duration) VALUES (NULLIF('${instructorID}', ''), '${data['addName']}', '${data['addDescription']}', '${data['addType']}', '${data['addDifficulty']}', '${convertedDuration}')`;
   db.pool.query(query1, function(error, rows, fields){
 
       // Check to see if there was an error
