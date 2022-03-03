04240b4ef76cea0141bd306f7375dc2fad150727
Collin Sidler <sidlerc@oregonstate.edu>
2022-03-02 20:19:03 -0800

    fixes Performs route file so that it grabs addTimeCompleted instead of timeCompleted

--- routes/performs.js
+++ routes/performs.js
@@ -1,42 +1,42 @@
-var db = require('../database/db-connector')
+var db = require("../database/db-connector");
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  let query1 = "SELECT * FROM Performs;";               // Define our query
-  db.pool.query(query1, function(error, rows, fields){    // Execute the query
-    res.render('performs', {data: rows});                  // Render the index.hbs file, and also send the renderer
-  })   
-})
+    let query1 = "SELECT * FROM Performs;"; // Define our query
+    db.pool.query(query1, function (error, rows, fields) {
+        // Execute the query
+        res.render("performs", { data: rows }); // Render the index.hbs file, and also send the renderer
+    });
+});
 
-router.post('/add-perform', function(req, res){
-  // Capture the incoming data and parse it back to a JS object
-  let data = req.body;
+router.post("/add-perform", function (req, res) {
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
 
-  let timeCompleted = "";
-  if (data['timeCompleted'] !== undefined){
-    instructorID = data['timeCompleted'];
-  }
+    let timeCompleted = "";
+    if (data["addTimeCompleted"] !== undefined) {
+        timeCompleted = data["addTimeCompleted"];
+    }
 
-  // Create the query and run it on the database
-  query1 = `INSERT INTO Performs(userID, videoID, completed, bookmarked, timeCompleted) VALUES ('${data['addUserID']}', '${data['addVideoID']}', '${data['addCompleted']}', '${data['addBookmarked']}', NULLIF('${timeCompleted}', ''))`;
-  db.pool.query(query1, function(error, rows, fields){
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Performs(userID, videoID, completed, bookmarked, timeCompleted) VALUES ('${data["addUserID"]}', '${data["addVideoID"]}', '${data["addCompleted"]}', '${data["addBookmarked"]}', NULLIF('${timeCompleted}', ''))`;
+    db.pool.query(query1, function (error, rows, fields) {
+        // Check to see if there was an error
+        if (error) {
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error);
+            res.status(400).send(
+                "Insufficient Parameters, Please verify that the userID and the videoID can be found in their respected tables."
+            );
+        }
 
-      // Check to see if there was an error
-      if (error) {
-
-          // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
-          console.log(error)
-          res.status(400).send("Insufficient Parameters, Please verify that the userID and the videoID can be found in their respected tables.");
-      }
-
-      // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
-      // presents it on the screen
-      else
-      {
-          res.redirect('/performs');
-      }
-  })
-})
+        // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
+        // presents it on the screen
+        else {
+            res.redirect("/performs");
+        }
+    });
+});
 module.exports = router;
