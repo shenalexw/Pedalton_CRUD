a80eb9d2f931b158e16bc8c96b52073b8f3e56b6
Alexander Shen <70786647+shenalexw@users.noreply.github.com>
2022-03-16 14:22:10 -0700

    testing public

--- server.js
+++ server.js
@@ -8,37 +8,36 @@ const port = process.env.PORT || 9242;
 
 // Parser
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 // Handlebars
 const { engine } = require('express-handlebars');
 var exphbs = require('express-handlebars');     // Import express-handlebars
-app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
+app.engine('.hbs', engine({ extname: ".hbs" }));  // Create an instance of the handlebars engine to process templates
 app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 
-// // This works for statically showing our html
-// app.use(express.static('public'));
+// This works for statically showing our html
+app.use(express.static('public'));
 
-// Routers
-const userRouter = require('./routes/users')
-const updateUserRouter = require('./routes/update-user')
-const instructorRouter = require('./routes/instructors')
-const membershipRouter = require('./routes/memberships')
-const performRouter = require('./routes/performs')
-const videoRouter = require('./routes/videos')
+// // Routers
+// const userRouter = require('./routes/users')
+// const updateUserRouter = require('./routes/update-user')
+// const instructorRouter = require('./routes/instructors')
+// const membershipRouter = require('./routes/memberships')
+// const performRouter = require('./routes/performs')
+// const videoRouter = require('./routes/videos')
 
-// Load the homepage
-app.get('/', function(req, res)
-{
-  res.render('index');                    // Note the call to render() and not send(). Using render() ensures the templating engine
-});
+// // Load the homepage
+// app.get('/', function (req, res) {
+//   res.render('index');                    // Note the call to render() and not send(). Using render() ensures the templating engine
+// });
 
-// Use routes
-app.use('/users', userRouter)
-app.use('/update-user', updateUserRouter)
-app.use('/instructors', instructorRouter)
-app.use('/memberships', membershipRouter)
-app.use('/performs', performRouter)
-app.use('/videos', videoRouter)
+// // Use routes
+// app.use('/users', userRouter)
+// app.use('/update-user', updateUserRouter)
+// app.use('/instructors', instructorRouter)
+// app.use('/memberships', membershipRouter)
+// app.use('/performs', performRouter)
+// app.use('/videos', videoRouter)
 
 app.listen(port);
